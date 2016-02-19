621ccd59dd9e53d2ab5637aa11528afef2ce17e2
Dennis Simpson <dennis@zonzorp.net>
2016-02-19 15:02:14 +0000

    Updates for week 6

--- bash/lab4/countdown.sh
+++ bash/lab4/countdown.sh
@@ -3,97 +3,29 @@
 # This script demonstrates how to trap signals and handle them using functions
 
 #### Variables
-programName=`basename "$0"` # used by error_functions.sh
-sleepTime=1 # delay used by sleeptime
-numberOfSleeps=30 # how many sleeps to wait for before quitting for inactivity
 
 #### Functions
 
-# This function will send an error message to stderr
+# The error-message function will send an error message to stderr
 # Usage:
 #   error-message ["some text to print to stderr"]
 #
-function error-message {
-        local prog=`basename $0`
-        echo "${prog}: ${1:-Unknown Error - a moose bit my sister once...}" >&2
-}
 
-# This function will send a message to stderr and exit with a failure status
-# Usage:
-#   error-exit ["some text to print" [exit-status]]
+# The usage function will display command syntax
 #
-function error-exit {
-        error-message "$1"
-        exit "${2:-1}"
-}
-function usage {
-        cat <<EOF
-Usage: ${programName} [-h|--help ] [-w|--waittime waittime] [-n|--waitcount waitcount]
-Default waittime is 1, waitcount is 30
-EOF
-}
 
 # Normally traps catch signals and do something useful or necessary with them
-
+# These functions are for use with traps for signals we want to catch
 # Quit if we get SIGQUIT, but let the user know why we are exiting
-function receivedQuit {
-    echo "Received SIGQUIT"
-    exit
-}
-
 # Squawk if we get SIGHUP
-function receivedHup {
-    echo "Received SIGHUP"
-}
+# If we get SIGINT, we reset the counter to where it started
 
-# If we get SIGINT, we reset the counter
-function receivedInt {
-    echo "Received SIGINT, resetting sleep count"
-    sleepCount=$numberOfSleeps
-}
-
-# trap the signals we care about
-trap receivedQuit SIGQUIT
-trap receivedHup SIGHUP
-trap receivedInt SIGINT
+# trap the signals we care about and use them to invoke the functions above
 
 #### Main Program
 
 # Process command line parameters
-while [ $# -gt 0 ]; do
-    case $1 in
-        -w | --waittime )
-            shift
-            sleepTime="$1"
-            ;;
-        -n | --waitcount)
-            shift
-            numberOfSleeps="$1"
-            ;;
-        -h | --help )
-            usage
-            exit
-            ;;
-        * )
-            usage
-            error-exit "$1 not a recognized option"
-    esac
-    shift
-done
 
-if [ ! $numberOfSleeps -gt 0 ]; then
-    error-exit "$numberOfSleeps is not a valid count of sleeps to wait for signals"
-fi
-
-if [ ! $sleepTime -gt 0 ]; then
-    error-exit "$sleepTime is not a valid time to sleep while waiting for signals"
-fi
-
-sleepCount=$numberOfSleeps
-
-while [ $sleepCount -gt 0 ]; do
-    echo "Waiting $sleepCount more times for signals"
-    sleep $sleepTime
-    sleepCount=$((sleepCount - 1))
-done
-echo "Wait counter expired, exiting peacefully"
+# display what is left in our count
+# sleep until it is time for the next display
+# end the script when the count reaches zero
--- bash/lab4/picstats.sh
+++ bash/lab4/picstats.sh
@@ -3,10 +3,19 @@
 #    how much space they use,
 #    and the sizes and names of the 3 largest files
 
-echo "The `find ~/Pictures -type f | wc -l` files in the ~/Pictures directory use:"
+echo -n There are `find ~/Pictures -type f | wc -l`
+echo " files in the ~/Pictures directory"
+
+# from joseph holland
+echo -n "There are "
+find ~/Pictures -type f | wc -l | xargs echo -n
+echo " files in the ~/Pictures directory"
+
+echo "The Pictures directory uses:"
 du -sh ~/Pictures
-echo
 
 echo "The 3 largest files in the ~/Pictures directory are:"
+# from Michael - du -h ~/Pictures/* | sort -hr | head -n3
+# from Simon - ls -lnhS ~/Pictures | grep ^- | head -3 
 cd ~/Pictures
 find . -type f -exec du -h {} \; | sort -h | tail -3
